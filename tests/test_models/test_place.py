fe620e1a1f9bee337c9bca05d7da7a16b1f19757
Ken K. Ganza <122864355+kennyg37@users.noreply.github.com>
2023-09-24 12:40:35 +0200

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,14 +1,9 @@
-#!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.place import Place
 
-
-class test_Place(test_basemodel):
-    """ """
+class TestPlace(TestBaseModel):
 
     def __init__(self, *args, **kwargs):
-        """ """
         super().__init__(*args, **kwargs)
         self.name = "Place"
         self.value = Place
