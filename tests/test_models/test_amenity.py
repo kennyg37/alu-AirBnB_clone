619498ebd1b7b80bf324e3104ecc585c4330ba78
Ken K. Ganza <122864355+kennyg37@users.noreply.github.com>
2023-09-24 12:37:35 +0200

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,20 +1,13 @@
-#!/usr/bin/python3
-""" """
 from tests.test_models.test_base_model import TestBaseModel
-
 from models.amenity import Amenity
 
-
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenity(TestBaseModel):
 
     def __init__(self, *args, **kwargs):
-        """ """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
-    def test_name2(self):
-        """ """
+    def test_name(self):
         new = self.value()
         self.assertEqual(type(new.name), str)
