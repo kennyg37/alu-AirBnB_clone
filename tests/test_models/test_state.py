a7adb8d4e419b08a56f32f24bd9b7bc4b023bd75
Ken K. Ganza <122864355+kennyg37@users.noreply.github.com>
2023-09-24 12:29:46 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,13 @@
-#!/usr/bin/python3
-""" """TestBaseModel
-from tests.test_models.test_base_model import 
+from tests.test_models.test_base_model import TestBaseModel
 from models.state import State
 
-
-class test_state(TestBaseModel):
-    """ """
+class TestState(TestBaseModel):
 
     def __init__(self, *args, **kwargs):
-        """ """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
-    def test_name3(self):
-        """ """
+    def test_name_type(self):
         new = self.value()
         self.assertEqual(type(new.name), str)
