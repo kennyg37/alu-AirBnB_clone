5a7fc6066f69d65b6ea275f1a16b310123a84415
Ken K. Ganza <122864355+kennyg37@users.noreply.github.com>
2023-09-24 12:41:25 +0200

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.review import Review
 
-
-class test_review(test_basemodel):
-    """ """
+class TestReview(TestBaseModel):
 
     def __init__(self, *args, **kwargs):
-        """ """
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
-
+        
     def test_place_id(self):
         """ """
         new = self.value()
